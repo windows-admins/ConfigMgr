1c957e6d06e745db8e553d25099aa5b8a66adab7
Ebag333 <ebagola@gmail.com>
2016-09-12 14:29:24 -0700

    Lots of updates

---
+++ OSD/Drivers/BurnDriverDatbaseWithFire.ps1
@@ -0,0 +1,23 @@
+﻿$drivers = Get-CMDrivers
+
+ForEach ($driver in $drivers)
+{
+    Remove-CMDrivers -Name $driver.LocalizedDisplayName -Force
+    Write-Host "Removing driver: $driver.LocalizedDisplayName"
+}
+
+$CMCategories = Get-CMCategory -Name $PackageName -CategoryType "DriverCategories"
+
+ForEach ($CMCategory in $CMCategories)
+{
+    Remove-CMCategory -Name $CMCategory.LocalizedCategoryInstanceName -Force
+    Write-Host "Removing driver category: $CMCategory.LocalizedCategoryInstanceName"
+}
+
+$CMDriverPackages = Get-CMDriverPackage
+
+ForEach ($CMDriverPackage in $CMDriverPackages)
+{
+    Remove-CMDriverPackage -Name $CMDriverPackage.Name -Force
+    Write-Host "Removing driver package: $CMDriverPackage.Name"
+}
