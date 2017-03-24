42c1a8fd501caca5127dee833d0a1b0b21f68f9b
LtBehr <ccw4@calvin.edu>
2017-03-24 12:49:00 -0400

    Update BurnDriverDatabaseWithFire.ps1
    Optimized Driver/Category/Package removal w/ InputObject. Should run a lot faster.

--- OSD/Drivers/BurnDriverDatabaseWithFire.ps1
+++ OSD/Drivers/BurnDriverDatabaseWithFire.ps1
@@ -1,23 +1,26 @@
-﻿$drivers = Get-CMDriver
+Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1" # Import the ConfigurationManager.psd1 module 
+
+$drivers = Get-CMDriver
 
 ForEach ($driver in $drivers)
 {
-    Remove-CMDriver -Name $driver.LocalizedDisplayName -Force
-    Write-Host "Removing driver: $driver.LocalizedDisplayName"
+Remove-CMDriver -InputObject $driver -Force
+Write-Host "Removing driver: $($driver.LocalizedDisplayName)"
+
 }
 
 $CMCategories = $CMCategories = Get-CMCategory -CategoryType "DriverCategories"
 
 ForEach ($CMCategory in $CMCategories)
 {
-    Remove-CMCategory -Name $CMCategory.LocalizedCategoryInstanceName -Force
-    Write-Host "Removing driver category: $CMCategory.LocalizedCategoryInstanceName"
+    Remove-CMCategory -InputObject $CMCategory -Force
+    Write-Host "Removing driver category: $($CMCategory.LocalizedCategoryInstanceName)"
 }
 
 $CMDriverPackages = Get-CMDriverPackage
 
 ForEach ($CMDriverPackage in $CMDriverPackages)
 {
-    Remove-CMDriverPackage -Name $CMDriverPackage.Name -Force
-    Write-Host "Removing driver package: $CMDriverPackage.Name"
+    Remove-CMDriverPackage -InputObject $CMDriverPackage -Force
+    Write-Host "Removing driver package: $($CMDriverPackage.Name)"
 }
