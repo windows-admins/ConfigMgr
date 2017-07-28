7b4a75b884b2305f2a46da6c05113ba86f95841c
Ebag333 <ebagola@gmail.com>
2017-07-28 06:38:25 -0700

    Configuration File Changes

---
+++ Misc/SCCM Scripts/Remove_SCCM_Client_GUID/Remove_SCCM_Client_Settings.cmd
@@ -0,0 +1,1 @@
+%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -File %~dp0Remove_SCCM_Client_Settings.PS1
---
+++ OSD/Configuration/Scripts/RemoveWin10Apps.ps1
@@ -0,0 +1,21 @@
+########################################
+# Remove Windows 10 Modern Apps (System)
+########################################
+
+# To find app names use: 
+# Get-AppxProvisionedPackage -online  | Select DisplayName, PackageName
+
+$appxpackages = ('Messaging', 'OneNote', 'OneConnect', 'SkypeApp', 'XboxApp')
+ 
+Foreach($package in $appxpackages){
+    try{
+	$packagename=(Get-AppxProvisionedPackage -online | ?{$_.DisplayName -like '*' + $package + '*'}).PackageName
+	DISM /online /remove-provisionedappxpackage /packagename:$packagename
+    }
+    catch{
+	# Do nothing
+	Write-Host "Critical error removing package: "
+	Write-Host $package
+    }
+}
+ 
