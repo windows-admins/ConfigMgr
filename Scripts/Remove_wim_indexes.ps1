fe02fa4be5ab36a66ec5e4da60f9a9c2df834a47
jones948 <32885348+jones948@users.noreply.github.com>
2018-02-19 12:35:00 -0800

    Clean up some comments.

--- Scripts/Remove_wim_indexes.ps1
+++ Scripts/Remove_wim_indexes.ps1
@@ -1,5 +1,5 @@
-﻿#Removes all indexes from an install.wim file except for specified index.
-#Useful if you use SCCM Servicing to inject updates and only use one index in the .wim. Otherwise SCCM services all indexes.
+﻿#Removes all indexes from an install.wim file except for the specified index.
+#Useful if you use SCCM Servicing to inject updates, but only deploy one index in the .wim (otherwise SCCM services all indexes).
 
 #Update the following in the script for your needs:
 # $image_path
@@ -11,11 +11,11 @@ $img_indexes = Get-WindowsImage -ImagePath $image_path
 
 #Create loop to remove indexes until we are left with one.
 do {
-    #Loop through each index and remove the index if not the designated edition.
+    #Loop through each index and remove the index if it is not the designated edition.
     foreach ($img_index in $img_indexes) {
         if ($img_index.ImageName -ne "Windows 10 Enterprise") {
         Remove-WindowsImage -ImagePath $image_path -index $img_index.ImageIndex
-        #Break out of the for loop, as we need to refresh the index list if we remove one of them.
+        #Break out of the for loop, as we need to refresh the index list. The indexes re-order once one is removed.
         break
         }
     }
