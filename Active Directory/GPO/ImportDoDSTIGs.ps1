3b9eac1de5a59af51efeb13c5f768ee7b7e44595
Ebag333 <ebagola@gmail.com>
2018-05-09 15:05:00 -0700

    Import DoD STIGs

---
+++ Active Directory/GPO/ImportDoDSTIGs.ps1
@@ -0,0 +1,52 @@
+# Map GUIDs to GPO display names
+<#
+.SYNOPSIS
+.DESCRIPTION
+.PARAMETER rootdir
+.EXAMPLE
+#>
+
+param(
+    [parameter(Mandatory=$true)]
+    [String]
+    $rootdir,
+    [parameter(Mandatory=$true)]
+    [String]
+    $version
+    )
+
+$results = @{}
+$STIGs = dir -Filter DoD* $rootdir
+
+ForEach ($STIG in $STIGs)
+{
+    $pathGPO = $rootdir + "\" + $STIG.Name + "\GPO"
+    $pathGPOs = $rootdir + "\" +  $STIG.Name + "\GPOs"
+
+    Write-Host $pathGPO
+    if (Test-Path -Path $pathGPO)
+    {
+        $GPOBackup = dir -Filter {*} $pathGPO
+        $name = "STIG" + $version + " " + $STIG.Name
+        Write-Host "Creating STIG: " $name
+        Import-GPO -BackupId $GPOBackup.Name -Path $pathGPO -TargetName $name -CreateIfNeeded
+    }
+    elseif (Test-Path -Path $pathGPOs)
+    {
+        $GPOBackups = dir -Filter {*} $pathGPOs
+        $count = 1
+        
+        ForEach ($GPOBackup in $GPOBackups)
+        {
+            $name = "STIG" + $version + " " + $STIG.Name + " [" + $count + "]"
+            Write-Host $name
+            Import-GPO -BackupId $GPOBackup.Name -Path $pathGPOs -TargetName $name -CreateIfNeeded
+            $count++
+        }
+    }
+    else
+    {
+        Write-Host "How did you get here for STIG: " $STIG.Name
+    }
+
+}
