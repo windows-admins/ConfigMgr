c465a7c23a92d2720d8e838c305b9b90cf906295
Chris Kibble <chris@christopherkibble.com>
2019-04-29 16:23:43 -0400

    Minor formatting fixes in SQL folder.

--- SQL/Find Asset and User Data by Installed Application Name (ARP).sql
+++ SQL/Find Asset and User Data by Installed Application Name (ARP).sql
@@ -26,10 +26,10 @@ select v_r_system.name0
      , v_add_remove_programs.Publisher0
      , v_add_remove_programs.displayname0
      , v_add_remove_programs.Version0
-	 , v_add_remove_programs.installdate0
-	 , v_R_User.Unique_User_Name0
-	 , v_R_User.cn0
-	 , v_R_User.Mail0
+     , v_add_remove_programs.installdate0
+     , v_R_User.Unique_User_Name0
+     , v_R_User.cn0
+     , v_R_User.Mail0
   from v_r_system
   join v_add_remove_programs
     on v_r_system.resourceid = v_add_remove_programs.resourceid
--- SQL/Find Assets by Primary User Email.sql
+++ SQL/Find Assets by Primary User Email.sql
@@ -1,31 +1,31 @@
 
 /*
 
-	Purpose: Find Assets by Primary User (Top Console User)
+     Purpose: Find Assets by Primary User (Top Console User)
 
-	Author(s): Chris Kibble (www.ChristopherKibble.com)
-	Contributor(s): 
-	Created: 2019-04-29
-	Last Updated: 2019-04-29
+     Author(s): Chris Kibble (www.ChristopherKibble.com)
+     Contributor(s): 
+     Created: 2019-04-29
+     Last Updated: 2019-04-29
 
-	To Use: 
-	
-		1) Connect to your SCCM database.
-		2) Modify the WHERE clause to include the user email address.
-		3) Execute.
+     To Use: 
+     
+          1) Connect to your SCCM database.
+          2) Modify the WHERE clause to include the user email address.
+          3) Execute.
 
-	Comments:
+     Comments:
 
-		* Requires that you be collecting cn0 and Mail0 during Active Directory User discovery or those fields may return errors.
+          * Requires that you be collecting cn0 and Mail0 during Active Directory User discovery or those fields may return errors.
 
 */
 
 
 select v_r_system.name0
      , v_r_system.Operating_System_Name_and0
-	 , v_R_User.Unique_User_Name0
-	 , v_R_User.cn0
-	 , v_R_User.Mail0
+     , v_R_User.Unique_User_Name0
+     , v_R_User.cn0
+     , v_R_User.Mail0
   from v_r_system
   join v_gs_system_console_usage
     on v_r_system.resourceid = v_gs_system_console_usage.resourceid
