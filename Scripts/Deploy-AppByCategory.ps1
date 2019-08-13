5af86c0c561abff9a9dedc24f23c7dd335735e45
Tesla <Tesla.SysA@gmail.com>
2019-08-13 10:27:34 -0700

    Update Deploy-AppByCategory.ps1
    Adding in code to load module.

--- Scripts/Deploy-AppByCategory.ps1
+++ Scripts/Deploy-AppByCategory.ps1
@@ -20,6 +20,30 @@
     Release Date: 2019-08-13
 #>
 
+# Site configuration
+$SiteCode = "CNT" # Site code 
+$ProviderMachineName = "cm.contoso.com" # SMS Provider machine name
+
+# Customizations
+$initParams = @{ }
+#$initParams.Add("Verbose", $true) # Uncomment this line to enable verbose logging
+#$initParams.Add("ErrorAction", "Stop") # Uncomment this line to stop the script on any errors
+
+# Do not change anything below this line
+
+# Import the ConfigurationManager.psd1 module 
+if ((Get-Module ConfigurationManager) -eq $null) {
+    Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1" @initParams 
+}
+
+# Connect to the site's drive if it is not already present
+if ((Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue) -eq $null) {
+    New-PSDrive -Name $SiteCode -PSProvider CMSite -Root $ProviderMachineName @initParams
+}
+
+# Set the current location to be the site code.
+Set-Location "$($SiteCode):\" @initParams
+
 # Set the required deployment args per collection
 $deployments = @{
     "HelpDesk Deployments"   = @{
