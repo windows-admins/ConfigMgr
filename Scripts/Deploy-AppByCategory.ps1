d81280f13ab8f6b72718676bbaa6600eeea22209
Tesla <Tesla.SysA@gmail.com>
2019-08-14 12:40:24 -0700

    Update Deploy-AppByCategory.ps1
    Moved static args up to the dynamic tables. Removed a redundant var section and called directly instead.

--- Scripts/Deploy-AppByCategory.ps1
+++ Scripts/Deploy-AppByCategory.ps1
@@ -7,7 +7,7 @@
     category queried.
 
     Originally designed to automatically deploy applications as available to a Helpdesk user collection, and
-    Available/Required (for Fast Channel installs) to a Machine Collection.
+    Available/Required (for Fast Channel installs) to a Machine Collection
 .INPUTS
     "$deployments" = Edit table with relevant collections, admin categories, and deployment settings
     "$newAppArgs" = Additional settings/defaults for application deployments
@@ -16,8 +16,10 @@
     Name:      Deploy-AppByCategory.ps1
     Author:    Vex
     Contributor: Chris Kibble (On a _massive_ level, thanks Chris!!!)
-    Version: 1.0.0
+    Version: 1.0.1
     Release Date: 2019-08-13
+    Updated:
+        Version 1.0.1: 2019-08-14
 #>
 
 # Site configuration
@@ -46,16 +48,20 @@ Set-Location "$($SiteCode):\" @initParams
 
 # Set the required deployment args per collection
 $deployments = @{
-    "HelpDesk Deployments"   = @{
+    "HelpDesk Deployments"             = @{
         "Category"         = "Helpdesk"
-        "Collections"      = @("User Test - HD")
+        "Collections"      = @("IT - Helpdesk")
         "ApprovalRequired" = $false
+        "DeployAction"     = "Install"
+        "DeployPurpose"    = "Available"
         "UserNotification" = "DisplayAll"
     }
-    "JustInTime Deployments" = @{
-        "Category"         = "Fast Channel App Deployment"
-        "Collections"      = @("Machine Test - Fast Channel App Deploy")
+    "Fast Channel Deployments" = @{
+        "Category"         = "Fast Channel App"
+        "Collections"      = @("Deploy - Fast Channel Deployment")
         "ApprovalRequired" = $true       
+        "DeployAction"     = "Install"
+        "DeployPurpose"    = "Available"
         "UserNotification" = "DisplaySoftwareCenterOnly"
     }
 }
@@ -68,15 +74,9 @@ $dists = Get-CMDistributionStatus
 
 # Loop through the $deployemnts
 ForEach ($deployment in $deployments.Keys) {
-    
-    # Set the variables based on the current deployment
-    $category = $deployments[$deployment].Category
-    $collections = $deployments[$deployment].Collections
-    $approval = $deployments[$deployment].ApprovalRequired
-    $userNotify = $deployments[$deployment].UserNotification
 
     # Pull a list of applications with that category
-    $appList = $apps.Where( { $_.LocalizedCategoryInstanceNames -contains $category })
+    $appList = $apps.Where( { $_.LocalizedCategoryInstanceNames -contains $deployments[$deployment].Category })
 
     # Loop over each application that should be deployed and ensure it is
     ForEach ($app in $appList) {
@@ -85,14 +85,14 @@ ForEach ($deployment in $deployments.Keys) {
         $distCount = ($dists | Where-Object { $_.ObjectID -eq $objectId }).NumberSuccess
 
         # Loop through the collections, if there are multiples
-        ForEach ($collection in $collections) {
+        ForEach ($collection in $deployments[$deployment].Collections) {
             
             $newAppArgs = @{
                 "Name"             = $app.LocalizedDisplayName
-                "DeployAction"     = "Install"
-                "DeployPurpose"    = "Available"
-                "ApprovalRequired" = $approval
-                "UserNotification" = $userNotify
+                "DeployAction"     = $deployments[$deployment].DeployAction
+                "DeployPurpose"    = $deployments[$deployment].DeployPurpose
+                "ApprovalRequired" = $deployments[$deployment].ApprovalRequired
+                "UserNotification" = $deployments[$deployment].UserNotification
                 "TimeBaseOn"       = "LocalTime"
                 "CollectionName"   = $collection
                 "WhatIf"           = $true
