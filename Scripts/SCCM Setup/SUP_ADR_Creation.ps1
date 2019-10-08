10a4b3b8aff2c062ec6fd2149ec0bd2be438d4fc
Ebag333 <ebagola@gmail.com>
2019-10-08 11:57:16 -0700

    Massively update script for better handling, cleaner, and all sorts of improvements

--- Scripts/SCCM Setup/SUP_ADR_Creation.ps1
+++ Scripts/SCCM Setup/SUP_ADR_Creation.ps1
@@ -2,15 +2,87 @@
 # Press 'F5' to run this script. Running this script will load the ConfigurationManager
 # module for Windows PowerShell and will connect to the site.
 #
-# This script was auto-generated at '6/3/2019 12:42:50 PM'.
 
 # Uncomment the line below if running in an environment where script signing is 
 # required.
 #Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
 
+# ____________________________________________________________________________________________
+#
+# CHANGE SETTINGS BELOW THIS LINE
+#
+# ____________________________________________________________________________________________
+
 # Site configuration
 $SiteCode = "CHQ" # Site code 
 $ProviderMachineName = "CM1.corp.contoso.com" # SMS Provider machine name
+
+# Set to the FQDN of where update package content source is stored
+$ContentSourcePath = "\\cm1.corp.contoso.com\Source\_Updates\"
+
+# Change this if you want to target a different set of collections.  The number of $collections and the number of $schedule must match.
+$collections = (”DOGFOOD1”,”DOGFOOD2”,”DOGFOOD3”,”DOGFOOD4”,”DOGFOOD5”,”DOGFOOD6”,”DOGFOOD7”,”DOGFOOD8”)
+
+# Add ADR rules to create multiple ADRs in a single pass.
+$ADRRules = (`
+    @{
+        ADRName = "Windows 10"; 
+        Products = 'Windows 10', 'Windows 10, version 1903 and later';
+        UpdateClassifications = "Critical Updates", "Definition Updates", "Feature Packs", "Security Updates","Service Packs", "Tools", "Update Rollups","Updates";
+        Title = "-en-gb"
+    },`
+    @{
+        ADRName = "Office 365"; 
+        Products = 'Office 365 Client';
+        UpdateClassifications = "Critical Updates", "Definition Updates", "Feature Packs", "Security Updates","Service Packs", "Tools", "Update Rollups","Updates";
+        Title = ""
+    }`
+)
+
+# Edit the schedule to your requirements.
+# It is not recommmended to have the schedule be longer than the ADR run cycle.
+# The schedule below assumes that the ADR only runs once a month.
+# Required is additive to available. This means that available 1, required 2 means that it's required on the third day, not the second.
+$schedule = (`
+    @{
+        Available = 1;
+        Required = 2
+    },
+    @{
+        Available = 2;
+        Required = 3
+    },
+    @{
+        Available = 3;
+        Required = 4
+    },
+    @{
+        Available = 8;
+        Required = 10
+    },
+    @{
+        Available = 10;
+        Required = 10
+    },
+    @{
+        Available = 12;
+        Required = 10
+    },
+    @{
+        Available = 14;
+        Required = 10
+    },
+    @{
+        Available = 16;
+        Required = 10
+    }
+)
+
+# ____________________________________________________________________________________________
+#
+# DO NOT CHANGE ANYTHING BELOW THIS LINE
+#
+# ____________________________________________________________________________________________
 
 # Customizations
 $initParams = @{}
@@ -33,138 +105,112 @@ if((Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue
 Set-Location "$($SiteCode):\" @initParams
 
 
-
-# New-CMSoftwareUpdateAutoDeploymentRule -CollectionId "DOGFOOD01" -DeploymentPackage $CMSoftwareUpdateDeploymentPackage -Name "DeploymentRule07" -ArticleId "117"
-
-# $CMSoftwareUpdateDeploymentPackage = New-CMSoftwareUpdateDeploymentPackage -Name "Windows 10" -Path "\\cm1.corp.contoso.com\Source\_Updates\Windows 10"
-
-# $CMCollection = Get-CMCollection -Id DOGFOOD1 -CollectionType Device
-
-# New-CMSoftwareUpdateAutoDeploymentRule -Collection $CMCollection -Product 'Windows 10', 'Windows 10, version 1903 and later' -Superseded $False -DeploymentPackage $CMSoftwareUpdateDeploymentPackage -Name "Windows 10" -AddToExistingSoftwareUpdateGroup $True -AllowRestart $False -AllowSoftwareInstallationOutsideMaintenanceWindow $False -AllowUseMeteredNetwork $False -EnabledAfterCreate $True -Enable $True -SendWakeupPacket $True -DeployWithoutLicense $False -RunType RunTheRuleAfterAnySoftwareUpdatePointSynchronization -SuppressRestartServer $True -SuppressRestartWorkstation $False -WriteFilterHandling $True -NoInstallOnRemote $True -NoInstallOnUnprotected $True -AvailableImmediately $True -DeadlineImmediately $True
-
-$ContentSourcePath = "\\cm1.corp.contoso.com\Source\_Updates\"
-
-$collections = (”DOGFOOD1”,”DOGFOOD2”,”DOGFOOD3”,”DOGFOOD4”,”DOGFOOD5”,”DOGFOOD6”,”DOGFOOD7”,”DOGFOOD8”)
-
-$ADRRules = (`
-@{
-    ADRName = "Windows 10"; 
-    Products = 'Windows 10', 'Windows 10, version 1903 and later';
-    UpdateClassifications = "Critical Updates", "Definition Updates", "Feature Packs", "Security Updates","Service Packs", "Tools", "Update Rollups","Updates";
-    Title = "-en-gb"
-},`
-@{
-    ADRName = "Office 365"; 
-    Products = 'Office 365 Client';
-    UpdateClassifications = "Critical Updates", "Definition Updates", "Feature Packs", "Security Updates","Service Packs", "Tools", "Update Rollups","Updates";
-    Title = ""
-}`
-)
-
-$schedule = (`
-@{
-    Available = 0;
-    Required = 3
-},
-@{
-    Available = 1;
-    Required = 5
-},
-@{
-    Available = 2;
-    Required = 7
-},
-@{
-    Available = 8;
-    Required = 22
-},
-@{
-    Available = 10;
-    Required = 24
-},
-@{
-    Available = 12;
-    Required = 26
-},
-@{
-    Available = 14;
-    Required = 28
-},
-@{
-    Available = 16;
-    Required = 30
-}
-)
-
 ForEach ($ADRRule in $ADRRules)
 {
-    $DeploymentPackagePath = New-Item -Path "FileSystem::$ContentSourcePath" -Name $ADRRule.ADRName -ItemType "directory"
-    $CMSoftwareUpdateDeploymentPackage = New-CMSoftwareUpdateDeploymentPackage -Name $ADRRule.ADRName -Path $DeploymentPackagePath.FullName -Priority High
+    Write-Host "Creating ADR rule for:" $ADRRule.ADRName
 
-    $CMSoftwareUpdateAutoDeploymentRule = New-CMSoftwareUpdateAutoDeploymentRule `
-    -Collection (Get-CMCollection -Id $collections[0] -CollectionType Device) `
-    -DeploymentPackage $CMSoftwareUpdateDeploymentPackage `
-    -Name $ADRRule.ADRName `
-    -AddToExistingSoftwareUpdateGroup $True `
-    -AlertTime 4 `
-    -AlertTimeUnit Weeks `
-    -AllowRestart $True `
-    -AllowSoftwareInstallationOutsideMaintenanceWindow $False `
-    -AllowUseMeteredNetwork $True `
-    -AvailableTime $schedule[0].Available `
-    -AvailableTimeUnit Days `
-    -DeadlineTime $schedule[0].Required `
-    -DeadlineTimeUnit Days `
-    -DeployWithoutLicense $True `
-    -DisableOperationManager $True `
-    -DownloadFromInternet $True `
-    -DownloadFromMicrosoftUpdate $True `
-    -EnabledAfterCreate $True `
-    -GenerateOperationManagerAlert $True `
-    -GenerateSuccessAlert $True `
-    -Language "English" `
-    -LanguageSelection "English" `
-    -NoInstallOnRemote $False `
-    -NoInstallOnUnprotected $True `
-    -Product $ADRRule.Products `
-    -RunType RunTheRuleAfterAnySoftwareUpdatePointSynchronization `
-    -SendWakeUpPacket $True `
-    -SuccessPercent 99 `
-    -Superseded $False `
-    -SuppressRestartServer $False `
-    -SuppressRestartWorkstation $False `
-    -Title $ADRRule.Title `
-    -UpdateClassification $ADRRule.UpdateClassifications `
-    -UseBranchCache $False `
-    -UserNotification DisplayAll `
-    -UseUtc $True `
-    -VerboseLevel AllMessages `
-    -WriteFilterHandling $True 
-
-    $Count=0
-    ForEach ($_ in $schedule)
+    if (Test-Path -Path (Join-Path -Path FileSystem::$ContentSourcePath -ChildPath $ADRRule.ADRName))
     {
-        If ($Count -eq 0)
-        {
-            $Count++
-            Continue
-        }
+        Write-Host $ADRRule.ADRName.ToString() "content source folder already exists."
+        $DeploymentPackagePath = Resolve-path -Path (Join-Path -Path FileSystem::$ContentSourcePath -ChildPath $ADRRule.ADRName)
+    }
+    else
+    {
+        $DeploymentPackagePath = New-Item -Path "FileSystem::$ContentSourcePath" -Name $ADRRule.ADRName -ItemType "directory"
+    }
 
-        New-CMAutoDeploymentRuleDeployment `
-        -InputObject $CMSoftwareUpdateAutoDeploymentRule `
-        -Collection (Get-CMCollection -Id $collections[$Count] -CollectionType Device) `
-        -AvailableTime $_.Available `
+    $CMSoftwareUpdateDeploymentPackage = Get-CMSoftwareUpdateDeploymentPackage -Name $ADRRule.ADRName
+    if ($CMSoftwareUpdateDeploymentPackage)
+    {
+        Write-Host $ADRRule.ADRName.ToString() "deployment package already exists."
+    }
+    else
+    {
+        $CMSoftwareUpdateDeploymentPackage = New-CMSoftwareUpdateDeploymentPackage -Name $ADRRule.ADRName -Path $DeploymentPackagePath.FullName -Priority High
+    }
+
+    if (Get-CMAutoDeploymentRule -Name $ADRRule.ADRName -Fast)
+    {
+        Write-Host $ADRRule.ADRName.ToString() "ADR Rule already exists."
+    }
+    else
+    {
+        $CMSoftwareUpdateAutoDeploymentRule = New-CMSoftwareUpdateAutoDeploymentRule `
+        -Collection (Get-CMCollection -Id $collections[0] -CollectionType Device) `
+        -DeploymentPackage $CMSoftwareUpdateDeploymentPackage `
+        -Name $ADRRule.ADRName `
+        -AddToExistingSoftwareUpdateGroup $True `
+        -AllowRestart $False `
+        -AvailableTime $schedule[0].Available `
         -AvailableTimeUnit Days `
-        -DeadlineTime $_.Required `
+        -DeadlineTime $schedule[0].Required `
         -DeadlineTimeUnit Days `
-        -EnableDeployment $True `
-        -UseUtc $True `
+        -DeployWithoutLicense $True `
+        -DownloadFromMicrosoftUpdate $True `
+        -AllowUseMeteredNetwork $False `
+        -EnabledAfterCreate $True `
+        -GenerateOperationManagerAlert $True `
+        -Language "English" `
+        -LanguageSelection "English" `
+        -NoInstallOnRemote $False `
+        -NoInstallOnUnprotected $True `
+        -Product $ADRRule.Products `
+        -RunType RunTheRuleAfterAnySoftwareUpdatePointSynchronization `
+        -Superseded $False `
+        -SuppressRestartServer $False `
+        -SuppressRestartWorkstation $False `
+        -Title $ADRRule.Title `
+        -UpdateClassification $ADRRule.UpdateClassifications `
+        -UseBranchCache $False `
+        -VerboseLevel AllMessages `
+        -UseUtc $False `
         -SendWakeupPacket $True `
         -UserNotification DisplayAll `
         -AllowSoftwareInstallationOutsideMaintenanceWindow $False `
-        -WriteFilterHandling $True
+        -WriteFilterHandling $True `
+        -SoftDeadlineEnabled $True `
+        -RequirePostRebootFullScan $True `
+        -SuccessPercentage 80 `
+        -GenerateSuccessAlert $True `
+        -AlertTime 7 `
+        -AlertTimeUnit Days
 
-        $Count++
+        $Count=0
+        ForEach ($_ in $schedule)
+        {
+            If ($Count -eq 0)
+            {
+                $Count++
+                Continue
+            }
+
+            $CMAutoDeploymentRuleDeployment = New-CMAutoDeploymentRuleDeployment `
+            -InputObject $CMSoftwareUpdateAutoDeploymentRule `
+            -Collection (Get-CMCollection -Id $collections[$Count] -CollectionType Device) `
+            -AvailableTime $_.Available `
+            -AvailableTimeUnit Days `
+            -AllowRestart $False `
+            -DeadlineTime $_.Required `
+            -DeadlineTimeUnit Days `
+            -EnableDeployment $True `
+            -UseUtc $False `
+            -SendWakeupPacket $True `
+            -UserNotification DisplayAll `
+            -AllowSoftwareInstallationOutsideMaintenanceWindow $False `
+            -WriteFilterHandling $True `
+            -SoftDeadlineEnabled $True `
+            -RequirePostRebootFullScan $True `
+            -SuccessPercentage 80 `
+            -GenerateSuccessAlert $True `
+            -AlertTime 7 `
+            -AlertTimeUnit Days
+            # These don't exist yet :(
+            # -DownloadFromInternet $False `
+            # -DownloadFromMicrosoftUpdate $True `
+
+            Write-Host "Created deployment for" $CMAutoDeploymentRuleDeployment.CollectionName
+
+            $Count++
+        }
     }
-
 }
