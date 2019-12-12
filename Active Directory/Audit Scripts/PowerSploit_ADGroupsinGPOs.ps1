1a9ef2b3bb3149e265940f27da5f7919e4530e57
Ebag333 <ebagola@gmail.com>
2019-12-12 09:02:39 -0600

    Add files via upload

---
+++ Active Directory/Audit Scripts/PowerSploit_ADGroupsinGPOs.ps1
@@ -0,0 +1,11 @@
+﻿Import-Module PowerSploit -ErrorAction SilentlyContinue
+
+If (-not (Get-Module -Name PowerSploit))
+{
+    Write-Host "Could not load PowerSploit module. Is it downloaded and installed?" -ForegroundColor Red
+    Write-Host "https://github.com/PowerShellMafia/PowerSploit" -ForegroundColor Cyan
+}
+
+Get-NetGPOGroup | 
+Select-Object @{expression={$_.GPODisplayName}; label=’GPO Display Name’}, @{expression={$_.GPOName}; label=’GUID’}, @{expression={$_.GPOType}; label=’GPO Type’}, @{expression={$_.GroupName}; label=’Group Name’}, @{expression={$_.GroupSID}; label=’Group SID’} | 
+Export-Csv -Path .\ADAudit_ADGroupsinGPOs.csv -NoTypeInformation
